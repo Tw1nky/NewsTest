110ebbf49f05171239d04c15a904360d41c44bca
Artyom Shklyarsky <49336634+Tw1nky@users.noreply.github.com>
2021-09-29 13:44:51 +0700

    Initial commit

---
+++ app/src/main/java/com/tw1nky/newstest/SplashScreenActivity.kt
@@ -0,0 +1,18 @@
+package com.tw1nky.newstest
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_screen)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+}
